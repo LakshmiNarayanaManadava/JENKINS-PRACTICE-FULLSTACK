64dd74fb2dff25112238853adead1ecc17f0b21c
narayana <mandavalakshminarayana446@gmail.com>
2025-09-16 08:30:49 +0530

    updated 2.0

--- HospitalManagement/src/main/java/com/klef/HospitalManagement/HospitalManagementApplication.java
+++ HospitalManagement/src/main/java/com/klef/HospitalManagement/HospitalManagementApplication.java
@@ -2,9 +2,10 @@ package com.klef.HospitalManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HospitalManagementApplication {
+public class HospitalManagementApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(HospitalManagementApplication.class, args);
